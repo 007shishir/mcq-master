3f0f192475bc1f87f7c7504e81f75d47eb405c60
saiful islam <shishir177753@gmail.com>
2019-02-18 18:43:18 +0600

    privacy policy, toolbar, submenu and fragment added

--- app/src/main/java/com/mcqmaster/saif_win10/mcqmaster/MoreOption.java
+++ app/src/main/java/com/mcqmaster/saif_win10/mcqmaster/MoreOption.java
@@ -1,16 +1,50 @@
 package com.mcqmaster.saif_win10.mcqmaster;
 
 import android.app.Activity;
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class MoreOption extends Activity {
+public class MoreOption extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_option);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                        new PrivacyPolicy()).addToBackStack(null).commit();
+                return true;
+            case R.id.item2:
+                startActivity(new Intent(MoreOption.this, MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
